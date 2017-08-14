de4967006a8593b2ae02be6d060c1efb1bf9a53c
Matt Tilley <mtilley255@gmail.com>
2017-08-13 20:40:29 -0500

    Still tinkering

--- JS/primary.js
+++ JS/primary.js
@@ -3,38 +3,12 @@ $(document).ready(function()
 	var a = document.getElementById("H201");
 	var b = document.getElementById("H202");
 	
-	var cycle = [a,b];
+	var test = ["THIS", "THAT", "ELSE", "WHAT"];
 	
-	var counter = 0;
-	
-	function intro()
+	test.forEach(function(item, index)
 	{
-		cycle.forEach(function()
-		{
-			$(cycle[counter])	.delay(1000)
-								.fadeIn(1000)
-								.fadeOut(1000)
-			counter++;
-					
-		});
-		
-		/*for (i=0; i<cycle.length; i++)
-		{
-			$(cycle[counter])	.delay(2000)
-							.fadeIn(2000)
-							.fadeOut(2000)
-			counter++
-			break
-			intro();
-		}		*/
-	}
-	
-	intro();
-		
-		
-		
-	
-	
-	
+		$(item).delay(3000);
+		console.log(item, index);
+	});
 	
 });
