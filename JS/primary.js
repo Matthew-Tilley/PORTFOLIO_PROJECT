f5b8eb18c56194f71baddc2c4fb5189253d80c93
Matt Tilley <mtilley255@gmail.com>
2017-08-13 20:04:23 -0500

    Code works - wrong effect.

--- JS/primary.js
+++ JS/primary.js
@@ -9,14 +9,24 @@ $(document).ready(function()
 	
 	function intro()
 	{
-		for (i=0; i<cycle.length; i++)
+		cycle.forEach(function()
+		{
+			$(cycle[counter])	.delay(1000)
+								.fadeIn(1000)
+								.fadeOut(1000)
+			counter++;
+					
+		});
+		
+		/*for (i=0; i<cycle.length; i++)
 		{
 			$(cycle[counter])	.delay(2000)
 							.fadeIn(2000)
 							.fadeOut(2000)
 			counter++
+			break
 			intro();
-		}		
+		}		*/
 	}
 	
 	intro();
