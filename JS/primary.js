4d4c7979f1c500f844c2ccf54c8da4b942a21e6f
Matt Tilley <mtilley255@gmail.com>
2017-08-13 19:59:10 -0500

    Butchering some JavaScript
    Should have been saving the previous "attempts" at this; just haven't figured out how I can get it to work right.

--- JS/primary.js
+++ JS/primary.js
@@ -1,4 +1,30 @@
 $(document).ready(function()
 {
+	var a = document.getElementById("H201");
+	var b = document.getElementById("H202");
+	
+	var cycle = [a,b];
+	
+	var counter = 0;
+	
+	function intro()
+	{
+		for (i=0; i<cycle.length; i++)
+		{
+			$(cycle[counter])	.delay(2000)
+							.fadeIn(2000)
+							.fadeOut(2000)
+			counter++
+			intro();
+		}		
+	}
+	
+	intro();
+		
+		
+		
+	
+	
+	
 	
 });
