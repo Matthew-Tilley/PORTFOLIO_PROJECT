b696a09a8611b8451b8c385bf4081bca244efa92
Matt Tilley <mtilley255@gmail.com>
2017-08-22 10:52:16 -0500

    Making lots of changes...
    Need to have a copy of this in GitHub in case something goes terribly wrong.

--- JS/primary.js
+++ JS/primary.js
@@ -1,4 +1,4 @@
-var H201 = document.getElementById("H201");
+/*var H201 = document.getElementById("H201");
 var H202 = document.getElementById("H202");
 var H203 = document.getElementById("H203");
 
@@ -6,20 +6,53 @@ var intro_1 = [H201, H202, H203];
 
 $(document).ready(function()
 {	
+	var a = document.getElementById("BOX04");
+	
+	$(a).show();
 	
 	
-	$.each(intro_1, function(i, item){
+	$("#BOX05").fadeIn(1000);*/
+	
+	
+	/*$.each(intro_1, function(i, item){
 		
 		setTimeout(function(){
 			$(item)	.delay(2000).fadeIn(1500)
 					.fadeOut(1500).delay(2000);
 		}, i * 3000);
+		*/
+	
 		
 	})
 	
 	
 	
-	$("#LOG01")	.delay(11000)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*$("#LOG01")	.delay(11000)
 				.fadeIn(3000)
 				.delay(1000)
 				.fadeOut(1000);
@@ -73,7 +106,7 @@ $(document).ready(function()
 	{
 		$("#BOX05")	.fadeIn(3000)
 					.css("display", "flex");
-	}, 21000);
+	}, 21000);*/
 	
 	
 	
