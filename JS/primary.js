e5be9cb435a01d4eb3603b363138f8161a69412e
Matt Tilley <mtilley255@gmail.com>
2017-08-26 10:11:06 -0500

    Created & tested variable H202
    Appears to be working properly.

--- JS/primary.js
+++ JS/primary.js
@@ -6,7 +6,8 @@ $(document).ready(function()
 	function intro()
 	{
 		var H201 = document.getElementById("H201");
-		$(H201).show();
+		var H202 = document.getElementById("H202");
+		$(H202).show();
 	}
 	
 	intro();
