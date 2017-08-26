26025af91c99ba995b86d5e8bac64859538b54a0
Matt Tilley <mtilley255@gmail.com>
2017-08-26 10:14:53 -0500

    Created & tested variable wordfade
    Appears to be working properly.

--- JS/primary.js
+++ JS/primary.js
@@ -8,7 +8,9 @@ $(document).ready(function()
 		var H201 = document.getElementById("H201");
 		var H202 = document.getElementById("H202");
 		var H203 = document.getElementById("H203");	
-		$(H203).show();
+		
+		var wordfade = [H201, H202, H203];
+		$(wordfade).show();
 	}
 	
 	intro();
