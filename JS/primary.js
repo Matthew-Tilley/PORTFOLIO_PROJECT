e9f8827b49b58b128dbc5118a5e1e5fc24d95bad
Matt Tilley <mtilley255@gmail.com>
2017-08-26 10:28:05 -0500

    Created & tested first setTimeout function.
    Appears to be working properly.

--- JS/primary.js
+++ JS/primary.js
@@ -10,7 +10,18 @@ $(document).ready(function()
 		var H203 = document.getElementById("H203");	
 		
 		var wordfade = [H201, H202, H203];
-		$(wordfade).show();
+		
+		$.each(wordfade, function(index, element)
+		{
+			setTimeout(function()
+			{
+				$(element)	.fadeIn(2000)
+							.fadeOut(2000);
+				
+			}, index * 4000);
+			
+		});
+		
 	}
 	
 	intro();
