f64a0a0ecf0c7baeffc2a5e3b31cf8ba7662f346
Matt Tilley <mtilley255@gmail.com>
2017-08-26 10:02:58 -0500

    JavaScript not working for some reason - clean slated.

--- JS/primary.js
+++ JS/primary.js
@@ -1,28 +1,10 @@
+
+
 $(document).ready(function()
 {
-	function intro()
-	{
-		var a = document.getElementById("H201");
-		var b = document.getElementById("H202");
-		var c = document.getElementById("H203");
-		
-		var cycler = [b,c];
-		
-		
-		$.each(fader, function(i, item)
-		{
-			
-			setTimeout(function()
-			{
-				$(item).fadeIn();
-			});
-			
-		});
-		
-	}
 	
-
 	
 	
 });
+
 
